bcd66f84cbe317e8fcc60ad8d8799b845de32206
Iago Rodrigues <yagomello16@gmail.com>
2025-05-29 16:25:44 -0300

    feat: print bookbot report

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-from stats import get_num_words, get_num_characters
+from stats import get_num_words, get_sorted_num_characters
 
 def main():
     file_path = "./books/frankenstein.txt"
-    print(get_num_words(file_path))
-    print(get_num_characters(file_path))
+    num_words = get_num_words(file_path)
+    characters_count = get_sorted_num_characters(file_path)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+
+    print("----------- Word Count ----------")
+    print(num_words)
+
+    print("--------- Character Count -------")
+    for character in characters_count:
+        char = str(character.get("char"))
+        num = character.get("num")
+        if not char.isalpha():
+            continue
+        print(f"{char}: {num}")
+
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,7 @@
+def sort_on(dict):
+    return dict["num"]
+
+
 def get_content(file_path: str) -> str:
     with open(file_path) as f:
         return f.read()
@@ -6,9 +10,10 @@ def get_content(file_path: str) -> str:
 def get_num_words(file_path: str) -> str:
     content = get_content(file_path)
     num_words = len(content.split())
-    return f"{num_words} words found in the document"
+    return f"Found {num_words} total words"
 
-def get_num_characters(file_path: str) -> object:
+
+def get_num_characters(file_path: str) -> dict[str, int]:
     characters_count: dict[str, int] = {}
     content = get_content(file_path).lower()
 
@@ -16,3 +21,14 @@ def get_num_characters(file_path: str) -> object:
         characters_count[character] = characters_count[character] + 1 if character in characters_count else 1;
 
     return characters_count
+
+
+def get_sorted_num_characters(file_path: str) -> list[dict[str, int | str]]:
+    characters_count_sorted_list: list[dict[str, int | str]] = []
+
+    characters_count = get_num_characters(file_path)
+    for char, num in characters_count.items():
+      characters_count_sorted_list.append({ "char": char, "num": num })
+
+    characters_count_sorted_list.sort(reverse=True, key=sort_on)
+    return characters_count_sorted_list
